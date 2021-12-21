da5c354cf1dc9175a7456763848a45f2ee8ae3de
Chalk <74837083+barmybrit@users.noreply.github.com>
2021-12-21 20:09:39 +0100

    Finished with Salary

--- Pages/Salary.cshtml.cs
+++ Pages/Salary.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace SKYSWebDemo2.Pages
 {
     public class SalaryModel : PageModel
     {
+        public string Name { get; set; }
+        public string CurrentDay { get; set; }
+
+        public decimal Salary { get; set; }
+        public string SalaryType { get; set; }
         public void OnGet()
         {
+            // There are no users in the system right now.
+            // This shows how we can filter on current user however
+            // The base class (PageModel) contains the identity User
+
+            if (base.User.Identity != null && !string.IsNullOrEmpty(base.User.Identity.Name))
+            { 
+                Name = base.User.Identity.Name;
+            }
+            else
+            {
+                Name = "Not logged in";
+            }
+
+
+            CurrentDay = DateTime.Now.DayOfWeek.ToString();
+            
+            
+            SalaryType = "monthly";
+            Salary = 12000;
         }
     }
 }
